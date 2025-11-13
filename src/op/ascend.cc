2633c4d15ed78b036f80cd5dbfc02e1e006cd7d6
Kenny_Ke <kejunchun2@huawei.com>
2025-11-13 15:28:07 +0800

    [Fuction] gemm_v0 split K axis

--- src/op/ascend.cc
+++ src/op/ascend.cc
@@ -117,6 +117,7 @@ Stmt AscendCopy::Lower(const LowerArgs &T, arith::Analyzer *analyzer) const {
     bool print_ub = false;
     bool gm2ub = false;
     bool ub2gm = false;
+    bool l0_dst_split = false;
   } config;
 
   std::stringstream ss;
@@ -129,11 +130,13 @@ Stmt AscendCopy::Lower(const LowerArgs &T, arith::Analyzer *analyzer) const {
   } else if (src.scope() == "shared.dyn" && dst.scope() == "wmma.matrix_a") {
     ss << "copy_l1_to_l0a";
     config.print_src_layout = true;
+    config.l0_dst_split = true;
     config.l12l0 = true;
   } else if (src.scope() == "shared.dyn" && dst.scope() == "wmma.matrix_b") {
     ss << "copy_l1_to_l0b";
     config.print_src_layout = true;
     config.l12l0 = true;
+    config.l0_dst_split = true;
   } else if (src.scope() == "wmma.accumulator" && dst.scope() == "global") {
     ss << "copy_l0c_to_gm";
     config.l0c2gm = true;
@@ -211,8 +214,7 @@ Stmt AscendCopy::Lower(const LowerArgs &T, arith::Analyzer *analyzer) const {
     } else if (src.scope() == "global") {
       ss << dst->shape[dst_ndim - 2] << ", " << dst->shape[dst_ndim - 1] << ">";
     } else {
-      ss << src->shape[src_ndim - 2] << ", " << src->shape[src_ndim - 1] << ", "
-         << dst->shape[dst_ndim - 2] << ", " << dst->shape[dst_ndim - 1] << ">";
+      ss << src->shape[src_ndim - 2] << ", " << src->shape[src_ndim - 1] << ">";
     }
   }
 
@@ -243,6 +245,12 @@ Stmt AscendCopy::Lower(const LowerArgs &T, arith::Analyzer *analyzer) const {
 
   if (config.needs_strideN) {
     new_args.push_back(strideN);
+  }
+
+  if (config.l0_dst_split) {
+    int dst_dim = dst->shape.size();
+    new_args.push_back(dst->shape[dst_dim - 2]);
+    new_args.push_back(dst->shape[dst_dim - 1]);
   }
 
   if (config.l0c2gm) {
